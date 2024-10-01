f6625b8dfd705adbd6d479834408e367a3b20428
Rob Nutland <Robbie.Nutland@hotmail.com>
2024-10-01 19:28:45 +0100

    Updates

--- Clean Power Plans/Clean Power Plans.cpp
+++ Clean Power Plans/Clean Power Plans.cpp
@@ -1,7 +1,14 @@
 // Clean Power Plans.cpp : Defines the entry point for the console application.
-//
 
-#include "stdafx.h"
+// Replaces the default power schemes with the current user's power schemes.
+// 
+// Replacing the default schemes enables users to use the Restore Defaults option in the Control Panel Power Options application to restore customized power scheme defaults instead
+// of the original Windows power scheme defaults.
+
+// The built executable must be ran as an administrator. Test by running the below from a command prompt: 
+//		>	powercfg -restoredefaultschemes
+
+//#include "stdafx.h"
 #include <Windows.h>
 #include <iostream>
 #include <string>
@@ -14,23 +21,33 @@ void ReplaceDefaultSchemes() {
 
 	PowerReplaceDefaultPowerSchemes PowerFunc;
 
-	HMODULE dll = LoadLibraryA("POWRPROF.DLL");
+	HMODULE dll = LoadLibraryA("PowrProf.dll");
 	if (NULL != dll) {
 		PowerFunc = (PowerReplaceDefaultPowerSchemes)GetProcAddress(dll, "PowerReplaceDefaultPowerSchemes");
 		if (NULL != PowerFunc) {
-			PowerFunc();
+			if (PowerFunc() == 0) {
+				cout << "Successfully replaced the default power schemes with the current user's power schemes." << endl;
+			}
+			else {
+				cout << "Error: Call to the 'PowerReplaceDefaultPowerSchemes' function failed." << endl;
+			}
 		}
 		else {
-			cout << "I can't load this function";
+			cout << "Error: Failed to load 'PowerReplaceDefaultPowerSchemes' function." << endl;
 		}
 	}
 	else {
-		cout << "I can't load this dll file";
+		cout << "Error: Failed to load 'PowrProf.dll' library." << endl;
 	}
+
+	return;
 }
 
 int main()
 {
 	ReplaceDefaultSchemes();
+	cout << "Press 'Enter' to exit...";
+	cin.get(); //pauses console window
+
 	return 0;
 }
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,6 @@
 Tool designed for Win7 as the registry location is inaccessible unless NT-AUTHORITY\SYSTEM.
+The executable must be ran as an administrator.
 
 1. Sets the user's current power plans as system defaults.
+2. Test by running the below from a command prompt: 
+   >	powercfg -restoredefaultschemes
